ea7cf7aa6b9380a62c58d758c080a142d0aaccf8
angelks <angelks4@live.com>
2017-11-22 16:37:07 +0800

    react-route测试

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import registerServiceWorker from './registerServiceWorker';//在生产环境做本地缓存的东西
+import registerServiceWorker from './registerServiceWorker'; //在生产环境做本地缓存的东西
 
 // 函数定义组件
 // function Welcome(props){
@@ -523,104 +523,184 @@ ReactDOM.render(
 // );
 
 
-// const scaleNames = {
-//   c: 'Celsius',
-//   f: 'Fahrenheit'
-// };
-//
-// function toCelsius(fahrenheit) {
-//   return (fahrenheit - 32) * 5 / 9;
-// }
-//
-// function toFahrenheit(celsius) {
-//   return (celsius * 9 / 5) + 32;
-// }
-//
-// function tryConvert(temperature, convert) {
-//   const input = parseFloat(temperature);
-//   if (Number.isNaN(input)) {
-//     return '';
-//   }
-//   const output = convert(input);
-//   const rounded = Math.round(output * 1000) / 1000;
-//   return rounded.toString();
-// }
-//
-// function BoilingVerdict(props) {
-//   if (props.celsius >= 100) {
-//     return <p>The water would boil.</p>;
-//   }
-//   return <p>The water would not boil.</p>;
-// }
-//
-// class TemperatureInput extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleChange = this.handleChange.bind(this);
-//   }
-//
-//
-//   handleChange(e) {
-//     this.props.onTemperatureChange(e.target.value);
-//   }
-//
-//   render() {
-//     const temperature = this.props.temperature;
-//     const scale = this.props.scale;
-//     return (
-//       <fieldset>
-//         <legend>Enter temperature in {scaleNames[scale]}:</legend>
-//         <input value={temperature}
-//                onChange={this.handleChange} />
-//       </fieldset>
-//     );
-//   }
-// }
-//
-// class Calculator extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-//     this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-//     this.state = {temperature: '', scale: 'c'};
-//   }
-//
-//   handleCelsiusChange(temperature) {
-//     this.setState({scale: 'c', temperature});
-//   }
-//
-//   handleFahrenheitChange(temperature) {
-//     this.setState({scale: 'f', temperature});
-//   }
-//
-//   render() {
-//     const scale = this.state.scale;
-//     const temperature = this.state.temperature;
-//     const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-//     const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-//
-//     return (
-//       <div>
-//         <TemperatureInput
-//           scale="c"
-//           temperature={celsius}
-//           onTemperatureChange={this.handleCelsiusChange} />
-//         <TemperatureInput
-//           scale="f"
-//           temperature={fahrenheit}
-//           onTemperatureChange={this.handleFahrenheitChange} />
-//         <BoilingVerdict
-//           celsius={parseFloat(celsius)} />
-//       </div>
-//     );
-//   }
-// }
-//
-// ReactDOM.render(
-//   <Calculator />,
-//   document.getElementById('root')
-// );
+/*const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
 
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = {temperature: '', scale: 'c'};
+  }
+
+  handleCelsiusChange(temperature) {
+    this.setState({scale: 'c', temperature});
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({scale: 'f', temperature});
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange} />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange} />
+        <BoilingVerdict
+          celsius={parseFloat(celsius)} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('root')
+);*/
+
+
+// import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topic = ({
+  match
+}) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Topics = ({
+  match
+}) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/topics">Topics</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+    </div>
+  </Router>
+)
+export default App;
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 
 
 registerServiceWorker();
